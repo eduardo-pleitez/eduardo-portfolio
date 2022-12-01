3acd6772976767858bbb5678e23c63abb96e29ef
Eduardo Mejia Pleitez <eduardo.77.1996@gmail.com>
2022-12-01 17:45:16 -0600

    :bug: Fix error page button bug

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,9 +6,11 @@ const NotFoundPage: React.FC<PageProps> = () => {
     <main className="h-screen w-full flex flex-col justify-center items-center bg-gray-900">
       <h2 className='text-white text-2xl mb-3'>Page not found</h2>
       <h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
-      <button className="mt-5 p-5 text-white border border-white">
-        <Link to="/">Go Home</Link>
-      </button>
+      <Link to="/">
+        <button className="mt-5 p-5 text-white border border-white">
+          Go Home
+        </button>
+      </Link>
     </main>
   );
 };
