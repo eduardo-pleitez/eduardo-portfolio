e85eaf1ad835fcb55daba6d5967fb26383781e28
Eduardo Mejia Pleitez <eduardo.77.1996@gmail.com>
2022-12-01 17:32:18 -0600

    :sparkles: Update error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,12 +3,16 @@ import { Link, HeadFC, PageProps } from 'gatsby';
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <div>
-      <h1>Page not found.</h1>
-    </div>
+    <main className="h-screen w-full flex flex-col justify-center items-center bg-gray-900">
+      <h2 className='text-white text-2xl mb-3'>Page not found</h2>
+      <h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
+      <button className="mt-5 p-5 text-white border border-white">
+        <Link to="/">Go Home</Link>
+      </button>
+    </main>
   );
 };
 
 export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>;
+export const Head: HeadFC = () => <title>Page not found</title>;
