27b07008d21e81730f566d5a94d2330a5102b4a9
Eduardo Mejia Pleitez <eduardo.77.1996@gmail.com>
2022-12-01 11:59:50 -0600

    :sparkles: Add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='absolute bottom-0 w-full p-4 bg-black shadow md:px-8 md:py-4 px-4'>
+      <p className='block text-white md:text-xl text-sm'>
+        Copyright © 2022 All Rights Reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
